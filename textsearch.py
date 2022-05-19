3fb5c2a908eb7339546db55c1cd379ab72e879b6
Luoshui <57760243+Luoshuiai@users.noreply.github.com>
2022-05-20 01:03:21 +0800

    get keys

--- textsearch.py
+++ textsearch.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import sqlite3
+from collections import Counter,OrderedDict
+import webbrowser
+
+from matplotlib.collections import Collection
+
+
 from textsplit import *
 
-mydb = sqlite3.connect('zotero.sqlite')
+Sqlite_path=input("Please input the path of your sqlite file:")
+mydb = sqlite3.connect(Sqlite_path)
 cursor = mydb.cursor()
-
+    
 def get_word_id(text, length):
     word_id=[]
     for i in range( 0 , length ):
@@ -23,6 +30,39 @@ def get_item_id(word_id):
         for i in range(0, len(data)):
             item_id_list.append(data[i][0])
 
-    print(item_id_list)  
     return item_id_list
 
+
+def get_key_list(item_id):
+    keys=[]
+    for i in range(0, len(item_id)):
+        cursor.execute('SELECT key FROM items WHERE itemID LIKE ?', (item_id[i],))
+        data = cursor.fetchall()
+        keys.append(data[0][0])
+    return keys
+
+def text_search(text):
+    text,length=split_zh_en(text)
+    word_id = get_word_id(text, length)
+    item_id = get_item_id(word_id)
+    key_list = get_key_list(item_id)
+    return key_list
+
+
+def de_duplicate(list_value):
+    list_de_duplicate=Counter(list_value)
+    return list_de_duplicate
+
+
+
+
+if __name__ == "__main__":
+    text=input("Please input the text you want to search:")
+    keys=text_search(text)
+    print(keys)
+    keys=de_duplicate(keys)
+    print(keys)
+    keys=list(keys.values())
+    print(keys)
+    # webbrowser.open("zotero://select/library/items/"+keys[0])
+    mydb.close()
